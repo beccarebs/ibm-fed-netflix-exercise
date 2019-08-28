18c433b2a7508ba4ab0eb010780aabdecf48f084
Rebecca Elmo <belmo@centerline.net>
2019-08-28 18:40:50 -0400

    adding json data for language switching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import EnglishContent from './data/en_US.json';
+import PigLatinContent from './data/la_PG.json';
 import './App.scss';
 
 function App() {
